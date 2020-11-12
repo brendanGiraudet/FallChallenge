88bcf57ac5176d0ed3ef8a61a09bb0c44011a804
baka <bakayarusama@gmail.com>
2020-11-12 22:08:30 +0100

    Get info and show in debug terminal

--- Source/Player.cs
+++ Source/Player.cs
@@ -9,47 +9,109 @@ using System.Collections.Generic;
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
-class Player
+public class Player
 {
     static void Main(string[] args)
     {
-        string[] inputs;
-
         // game loop
         while (true)
         {
-            int actionCount = int.Parse(Console.ReadLine()); // the number of spells and recipes in play
-            for (int i = 0; i < actionCount; i++)
-            {
-                inputs = Console.ReadLine().Split(' ');
-                int actionId = int.Parse(inputs[0]); // the unique ID of this spell or recipe
-                string actionType = inputs[1]; // in the first league: BREW; later: CAST, OPPONENT_CAST, LEARN, BREW
-                int delta0 = int.Parse(inputs[2]); // tier-0 ingredient change
-                int delta1 = int.Parse(inputs[3]); // tier-1 ingredient change
-                int delta2 = int.Parse(inputs[4]); // tier-2 ingredient change
-                int delta3 = int.Parse(inputs[5]); // tier-3 ingredient change
-                int price = int.Parse(inputs[6]); // the price in rupees if this is a potion
-                int tomeIndex = int.Parse(inputs[7]); // in the first two leagues: always 0; later: the index in the tome if this is a tome spell, equal to the read-ahead tax
-                int taxCount = int.Parse(inputs[8]); // in the first two leagues: always 0; later: the amount of taxed tier-0 ingredients you gain from learning this spell
-                bool castable = inputs[9] != "0"; // in the first league: always 0; later: 1 if this is a castable player spell
-                bool repeatable = inputs[10] != "0"; // for the first two leagues: always 0; later: 1 if this is a repeatable player spell
-            }
-            for (int i = 0; i < 2; i++)
-            {
-                inputs = Console.ReadLine().Split(' ');
-                int inv0 = int.Parse(inputs[0]); // tier-0 ingredients in inventory
-                int inv1 = int.Parse(inputs[1]);
-                int inv2 = int.Parse(inputs[2]);
-                int inv3 = int.Parse(inputs[3]);
-                int score = int.Parse(inputs[4]); // amount of rupees
-            }
+            var commands = GetCommands();
+            System.Console.Error.WriteLine("***** COMMANDS *****");
+            commands.ForEach(System.Console.Error.WriteLine);
 
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
-
+            var inventories = GetInventories();
+            System.Console.Error.WriteLine("***** INVENTORIES *****");
+            inventories.ForEach(System.Console.Error.WriteLine);
 
             // in the first league: BREW <id> | WAIT; later: BREW <id> | CAST <id> [<times>] | LEARN <id> | REST | WAIT
             Console.WriteLine("BREW 0");
         }
+    }
+    static List<Inventory> GetInventories()
+    {
+        var inventories = new List<Inventory>();
+
+        for (int i = 0; i < 2; i++)
+        {
+            var inputs = Console.ReadLine().Split(' ');
+            inventories.Add(new Inventory
+            {
+                NumberOfBlueIngredient = int.Parse(inputs[0]),
+                NumberOfGreenIngredient = int.Parse(inputs[1]),
+                NumberOfOrangeIngredient = int.Parse(inputs[2]),
+                NumberOfYellowIngredient = int.Parse(inputs[3]),
+                Score = int.Parse(inputs[4])
+            });
+        }
+
+        return inventories;
+    }
+    static List<Command> GetCommands()
+    {
+        var commands = new List<Command>();
+        int actionCount = int.Parse(Console.ReadLine()); // the number of spells and recipes in play
+        for (int i = 0; i < actionCount; i++)
+        {
+            var inputs = Console.ReadLine().Split(' ');
+
+            commands.Add(new Command{
+                Id = int.Parse(inputs[0]),
+                Type = inputs[1],
+                NumberOfBlueIngredient = int.Parse(inputs[2]),
+                NumberOfGreenIngredient = int.Parse(inputs[3]),
+                NumberOfOrangeIngredient = int.Parse(inputs[4]),
+                NumberOfYellowIngredient = int.Parse(inputs[5]),
+                Price = int.Parse(inputs[6]),
+                TomeIndex = int.Parse(inputs[7]),
+                TaxCount = int.Parse(inputs[8]),
+                Castable = inputs[9] != "0",
+                Repeatable = inputs[10] != "0"
+            });
+        }
+
+        return commands;
+    }
+}
+public class Inventory
+{
+    public int NumberOfBlueIngredient { get; set; }
+    public int NumberOfGreenIngredient { get; set; }
+    public int NumberOfOrangeIngredient { get; set; }
+    public int NumberOfYellowIngredient { get; set; }
+    public int Score { get; set; }
+
+    public override string ToString()
+    {
+        return $"blue : {NumberOfBlueIngredient} " +
+            $"green : {NumberOfGreenIngredient} " +
+            $"orange : {NumberOfOrangeIngredient} " +
+            $"yellow : {NumberOfYellowIngredient} " +
+            $"Score : {Score} ";
+    }
+}
+public class Command
+{
+    public int Id { get; set; }
+    public string Type { get; set; }
+    public int NumberOfBlueIngredient { get; set; }
+    public int NumberOfGreenIngredient { get; set; }
+    public int NumberOfOrangeIngredient { get; set; }
+    public int NumberOfYellowIngredient { get; set; }
+    public int Price { get; set; }
+    public int TomeIndex { get; set; }
+    public int TaxCount { get; set; }
+    public bool Castable { get; set; }
+    public bool Repeatable { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id: {Id} " + 
+            $"Type : {Type} " +
+            $"blue : {NumberOfBlueIngredient} " +
+            $"green : {NumberOfGreenIngredient} " +
+            $"orange : {NumberOfOrangeIngredient} " +
+            $"yellow : {NumberOfYellowIngredient} " +
+            $"Price : {Price} ";
     }
 }
