9bf3f2287a8c8e4b569602ad5ee363f35cec98aa
baka <bakayarusama@gmail.com>
2020-11-14 18:01:41 +0100

    commit to save pending changes

--- Source/Player.cs
+++ Source/Player.cs
@@ -23,7 +23,7 @@ namespace FallChallenge
 
                 var castCommands = commands.Where(c => c.Type.Equals(CommandType.Cast)).ToList();
                 System.Console.Error.WriteLine("***** CASTS *****");
-                brewCommands.ForEach(System.Console.Error.WriteLine);
+                castCommands.ForEach(System.Console.Error.WriteLine);
 
                 var inventories = GetInventories();
                 System.Console.Error.WriteLine("***** INVENTORIES *****");
@@ -31,20 +31,89 @@ namespace FallChallenge
 
                 var myIventory = inventories.First();
 
-                var bestCommand = GetTheBestCommand(myIventory, brewCommands);
+                var doableCommand = GetTheDoableCommand(myIventory, brewCommands);
 
-                // in the first league: BREW <id> | WAIT; later: BREW <id> | CAST <id> [<times>] | LEARN <id> | REST | WAIT
-                Console.WriteLine($"BREW {bestCommand.Id}");
+                if (doableCommand != null)
+                {
+                    Console.WriteLine($"{doableCommand.GetCommandTypeAsString()} {doableCommand.Id}");
+                    continue;
+                }
+
+                var mostDoableCommand = GetTheMostDoableCommand(myIventory, brewCommands);
+                var bestCast = GetTheBestCast(myIventory, mostDoableCommand, castCommands);
+
+                if (bestCast.Castable)
+                {
+                    Console.WriteLine($"{bestCast.GetCommandTypeAsString()} {bestCast.Id}");
+                }
+
+                Console.WriteLine($"REST");
             }
         }
-        public static Command GetTheBestCommand(Inventory inventory, List<Command> commands)
+
+        public static Command GetTheBestCast(Inventory inventory, Command command, List<Command> castCommands)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Command GetTheMostDoableCommand(Inventory inventory, List<Command> commands)
+        {
+            var theMostDoableCommand = commands.First();
+            var bestNumberOfAction = GetNumberOfActionToBuildMissingIngredients(inventory, theMostDoableCommand);
+            foreach (var command in commands)
+            {
+                var numberOfAction = GetNumberOfActionToBuildMissingIngredients(inventory, command);
+                if(bestNumberOfAction > numberOfAction)
+                {
+                    theMostDoableCommand = command;
+                    bestNumberOfAction = numberOfAction;
+                }
+            }
+            return theMostDoableCommand;
+        }
+
+        private static int GetNumberOfActionToBuildMissingIngredients(Inventory inventory, Command command)
+        {
+            var numberOfaction = 0;
+
+            var numberOfMissingBlueIngredients = inventory.NumberOfBlueIngredient + command.NumberOfBlueIngredient;
+
+            var numberOfMissingGreenIngredients = inventory.NumberOfGreenIngredient + command.NumberOfGreenIngredient;
+
+            if(numberOfMissingBlueIngredients > 0)
+                numberOfMissingBlueIngredients = 0;
+
+            numberOfaction+= numberOfMissingBlueIngredients;
+
+            
+            if(numberOfMissingGreenIngredients > 0)
+                numberOfMissingGreenIngredients = 0;
+            else
+                numberOfaction += numberOfMissingGreenIngredients * 2 - numberOfMissingBlueIngredients;
+
+            var numberOfMissingOrangeIngredients = inventory.NumberOfOrangeIngredient + command.NumberOfOrangeIngredient;
+            if(numberOfMissingOrangeIngredients > 0)
+                numberOfMissingOrangeIngredients = 0;
+            else
+                numberOfaction += numberOfMissingOrangeIngredients * 3 - ;
+
+            var numberOfMissingYellowIngredients = inventory.NumberOfYellowIngredient + command.NumberOfYellowIngredient;
+            if(numberOfMissingYellowIngredients > 0)
+                numberOfMissingYellowIngredients = 0;
+            else
+                numberOfaction = numberOfMissingYellowIngredients * 4;
+
+            return numberOfaction;
+        }
+
+        public static Command GetTheDoableCommand(Inventory inventory, List<Command> commands)
         {
             var sortedCommandsByPrice = commands.OrderByDescending(c => c.Price);
-            
+
             return sortedCommandsByPrice.FirstOrDefault(c => HaveEnoughIngredient(inventory, c));
         }
 
-        private static bool HaveEnoughIngredient(Inventory inventory, Command command)
+        public static bool HaveEnoughIngredient(Inventory inventory, Command command)
         {
             return inventory.NumberOfBlueIngredient - command.NumberOfBlueIngredient >= 0
                 && inventory.NumberOfGreenIngredient - command.NumberOfGreenIngredient >= 0
@@ -79,7 +148,8 @@ namespace FallChallenge
             {
                 var inputs = Console.ReadLine().Split(' ');
 
-                commands.Add(new Command{
+                commands.Add(new Command
+                {
                     Id = int.Parse(inputs[0]),
                     Type = GetCommandType(inputs[1]),
                     NumberOfBlueIngredient = int.Parse(inputs[2]),
@@ -137,6 +207,18 @@ namespace FallChallenge
     {
         public int Id { get; set; }
         public CommandType Type { get; set; }
+
+        public string GetCommandTypeAsString()
+        {
+            switch (Type)
+            {
+                case CommandType.Brew:
+                default:
+                    return "BREW";
+                case CommandType.Cast:
+                    return "CAST";
+            }
+        }
         public int NumberOfBlueIngredient { get; set; }
         public int NumberOfGreenIngredient { get; set; }
         public int NumberOfOrangeIngredient { get; set; }
@@ -149,7 +231,7 @@ namespace FallChallenge
 
         public override string ToString()
         {
-            return $"Id: {Id} " + 
+            return $"Id: {Id} " +
                 $"Type : {Type} " +
                 $"blue : {NumberOfBlueIngredient} " +
                 $"green : {NumberOfGreenIngredient} " +
--- Test/Player.Tests.cs
+++ Test/Player.Tests.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 using System.Collections.Generic;
 
@@ -7,7 +6,7 @@ namespace FallChallenge.Tests
     public class PlayerTests
     {
         [Fact]
-        public void ShouldHaveBestCommand()
+        public void ShouldGetTheDoableCommand()
         {
             // Arrange
             var inventory = new Inventory
@@ -68,7 +67,126 @@ namespace FallChallenge.Tests
             };
 
             // Act
-            var actualCommand = Player.GetTheBestCommand(inventory, commands);
+            var actualCommand = Player.GetTheDoableCommand(inventory, commands);
+
+            // Assert
+            Assert.Equal(expectedCommand.Id, actualCommand.Id);
+        }
+
+        [Fact]
+        public void ShouldHaveTrueWhenGetCommandWithNotEnoughIngredient()
+        {
+            // Arrange
+            var inventory = new Inventory
+            {
+                NumberOfBlueIngredient = 3,
+                NumberOfGreenIngredient = 0,
+                NumberOfOrangeIngredient = 0,
+                NumberOfYellowIngredient = 0
+            };
+            var bestCommand = new Command
+            {
+                NumberOfBlueIngredient = -2,
+                NumberOfGreenIngredient = -2,
+                NumberOfOrangeIngredient = 0,
+                NumberOfYellowIngredient = 0,
+                Price = 19
+            };
+
+            // Act
+            var haveEnoughIngredient = Player.HaveEnoughIngredient(inventory, bestCommand);
+
+            // Assert
+            Assert.True(haveEnoughIngredient);
+        }
+
+        [Fact]
+        public void ShouldGetTheNumberOfACtionToBuildMissingIngredient()
+        {
+            // Arrange
+            var inventory = new Inventory
+            {
+                NumberOfBlueIngredient = 3,
+                NumberOfGreenIngredient = 0,
+                NumberOfOrangeIngredient = 0,
+                NumberOfYellowIngredient = 0
+            };
+            var bestCommand = new Command
+            {
+                NumberOfBlueIngredient = -2,
+                NumberOfGreenIngredient = -2,
+                NumberOfOrangeIngredient = 0,
+                NumberOfYellowIngredient = 0,
+                Price = 19
+            };
+            var expectedNumberOfAction = 3;
+
+            // Act
+            var actualNumberOfAction = Player.GetNumberOfActionToBuildMissingIngredients(inventory, bestCommand);
+
+            // Assert
+            Assert.Equal(expectedNumberOfAction, actualNumberOfAction);
+        }
+
+        [Fact]
+        public void ShouldGetTheMostDoableCommand()
+        {
+            // Arrange
+            var inventory = new Inventory
+            {
+                NumberOfBlueIngredient = 3,
+                NumberOfGreenIngredient = 0,
+                NumberOfOrangeIngredient = 0,
+                NumberOfYellowIngredient = 0
+            };
+            var expectedCommand = new Command
+            {
+                Id = 42,
+                NumberOfBlueIngredient = -2,
+                NumberOfGreenIngredient = -2,
+                NumberOfOrangeIngredient = 0,
+                NumberOfYellowIngredient = 0
+            };
+            var commands = new List<Command>
+            {
+                new Command
+                {
+                    Id = 61,
+                    NumberOfBlueIngredient = 0,
+                    NumberOfGreenIngredient = 0,
+                    NumberOfOrangeIngredient = 0,
+                    NumberOfYellowIngredient = -4
+                },
+                new Command
+                {
+                    Id = 48,
+                    NumberOfBlueIngredient = 0,
+                    NumberOfGreenIngredient = -2,
+                    NumberOfOrangeIngredient = -2,
+                    NumberOfYellowIngredient = 0
+                },
+                new Command
+                {
+                    Id = 77,
+                    NumberOfBlueIngredient = -1,
+                    NumberOfGreenIngredient = -1,
+                    NumberOfOrangeIngredient = -1,
+                    NumberOfYellowIngredient = -3
+                },
+                new Command
+                {
+                    Id = 66,
+                    NumberOfBlueIngredient = -2,
+                    NumberOfGreenIngredient = -1,
+                    NumberOfOrangeIngredient = 0,
+                    NumberOfYellowIngredient = -1,
+                    Price = 9
+                },
+                expectedCommand
+            };
+
+            // Act
+            var actualCommand = Player.GetTheMostDoableCommand(inventory, commands);
 
             // Assert
             Assert.Equal(expectedCommand.Id, actualCommand.Id);
